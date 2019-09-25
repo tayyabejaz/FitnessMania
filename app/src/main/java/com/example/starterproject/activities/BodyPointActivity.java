b9cce4775db925c944c6af346b2a0c7be27630cb
Tayyab Ejaz <tbjz497@gmail.com>
2019-09-26 00:32:46 +0500

    Completed Second Screens and Partial First Screen

--- app/src/main/java/com/example/starterproject/activities/BodyPointActivity.java
+++ app/src/main/java/com/example/starterproject/activities/BodyPointActivity.java
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil;
 
 import com.example.starterproject.R;
 import com.example.starterproject.databinding.ActivityBodyPointBinding;
+import com.example.starterproject.helper.AdsManager;
 
 public class BodyPointActivity extends AppCompatActivity {
 
@@ -17,7 +18,53 @@ public class BodyPointActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_body_point);
 
-        binding.activityTitle.setText(getIntent().getExtras().getString("title"));
+        updateUI(getIntent().getExtras().getString("title"));
+
+
+    }
+
+    private void updateUI(String title){
+
+        switch (title){
+            case "Abs Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_abs_body);
+                break;
+
+            case "Arms Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_arms_body);
+                break;
+
+            case "Back Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_back_body);
+                break;
+
+            case "Chest Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_chest_body);
+
+                break;
+
+            case "Hips Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_hips_body);
+
+                break;
+
+            case "Legs Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_legs_body);
+
+                break;
+
+            case "Buttocks Training":
+                binding.activityTitle.setText(title);
+                binding.bodyPointMainImage.setImageResource(R.drawable.fitnes_mania_buttocks_body);
+
+                break;
+        }
 
     }
 }
